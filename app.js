3577e0570fecb635e24168e96df34da5bc1eb535
rachit2501 <rac.sri25@gmail.com>
2020-04-10 13:47:20 +0530

    production

--- app.js
+++ app.js
@@ -3,35 +3,35 @@ const path = require("path");
 const logger = require("morgan");
 const express = require("express");
 const body = require("body-parser");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const session = require("express-session");
-const MongoStore = require("connect-mongodb-session")(session);
+// const MongoStore = require("connect-mongodb-session")(session);
 const app = express();
 const server = require("./graphQl/integrate");
 const cors = require("cors");
 
 app.use(body.urlencoded({ extended: false }));
 app.use(cors());
-const mongoURI = "mongodb://localhost:27017/covidExtricate";
-const loc = path.join(__dirname,"build","index.html");
-mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to Mongodb"))
-  .catch(() => console.log("Error Connecting to MongoDB"));
+// const mongoURI = "mongodb://localhost:27017/covidExtricate";
+// const loc = path.join(__dirname,"build","index.html");
+// mongoose
+//   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("Connected to Mongodb"))
+//   .catch(() => console.log("Error Connecting to MongoDB"));
 
 // app.use(logger("combined"));
 app.use(express.static('build'))
-app.use(
-  session({
-    resave: true,
-    secret: "covidextricate",
-    saveUninitialized: true,
-    store: new MongoStore({
-      url: mongoURI,
-      autoReconnect: true,
-    }),
-  })
-);
+// app.use(
+//   session({
+//     resave: true,
+//     secret: "covidextricate",
+//     saveUninitialized: true,
+//     store: new MongoStore({
+//       url: mongoURI,
+//       autoReconnect: true,
+//     }),
+//   })
+// );
 
 server.applyMiddleware({ app, path: "/api" });
 

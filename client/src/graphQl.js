aa3db40fa4001cec1ca9ff4a983282a79b9e881a
rachit2501 <rac.sri25@gmail.com>
2020-04-20 00:04:37 +0530

    merger

--- client/src/graphQl.js
+++ client/src/graphQl.js
@@ -4,7 +4,7 @@ import { HttpLink } from "apollo-link-http";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: "http://localhost:4000/api",
+  uri: "http://covidextricate.herokuapp.com/api",
 });
 
 const client = new ApolloClient({
